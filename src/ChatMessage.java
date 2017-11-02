d27f8120ac47a17c62261f01c00262e6d2dae30a
Rezaul Islam <rejaulcuet88@gmail.com>
2017-11-02 14:22:46 +0600

    Initial Commit

---
+++ src/ChatMessage.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+
+public class ChatMessage implements Serializable {
+
+	protected static final long serialVersionUID = 1112122200L;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+	private int type;
+	private String message;
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	// getters
+	int getType() {
+		return type;
+	}
+	String getMessage() {
+		return message;
+	}
+}
